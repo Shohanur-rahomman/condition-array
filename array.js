a43628cb73ef2cfe77bc9e54b242f5c2fa5451c2
Shohanur-rahomman <mdshohanur757@gmail.com>
2025-02-07 16:51:53 +0600

    all set

--- array.js
+++ array.js
@@ -1,1 +1,30 @@
-const friends = ['babul','abul',]
+const friends = ['babul','abul','kalam','joy'];
+const brother = ['babul','abul','kalam','joy'];
+
+// array check
+console.log(Array.isArray(friends));
+// array length check
+console.log(friends.length);
+
+// find array element number
+console.log(friends.indexOf('abul'));
+
+// best way to find array element
+console.log(friends.includes('abul'));
+
+// array last element add
+friends.push('jamal');
+
+// remove last element
+friends.pop();
+
+// array fast element remove
+const removeElement = friends.shift();
+console.log(removeElement);
+friends.shift();
+console.log(friends);
+
+// array fast element add
+friends.unshift('joynal');
+
+console.log(friends);
---
+++ slice-splice.js
@@ -0,0 +1,8 @@
+const number = [1,2,3,4,5,6,7,8,9];
+
+// const newNumber = number.slice(1,4);
+// const update = number.splice(1,2);
+const update = number.splice(2,8);
+console.log(update);
+console.log(number);
+// console.log(newNumber);
